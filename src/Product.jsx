2bb93bd5bbac8f399acd949d5f45a9cf9ac016eb
Ankan Basu <ankanbasu10@gmail.com>
2025-08-04 12:26:09 +0530

    Day2 of REACT REVISE

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,10 +1,28 @@
 import "./Product.css";
 
-function Product() {
+// function Product(props) {
+    function Product({title, price=1, features = [], features2}) {
+     
+    //  const list = features.map((feature) => <li>{feature}</li>);
+    
+    // let isDiscount = price > 30000 ? "Discount of 5%" : "";
     return (
         <div className="Product">
-            <h3>Product Title</h3>
-            <h5>Product description</h5>
+            {/* <h3>Product Title</h3> */}
+            {/* <h3>{props.title}</h3> */}
+            <h3>{title}</h3>
+            {/* <h5>Price : {price / 2}</h5> */}
+            <h5>Price : {price}</h5>
+            
+            {/* {price > 30000 ? <p>"Discount of 5%</p> : <a href="/">Get Discount</a>} */}
+
+            {price > 30000 && <p>"Discount of 5%</p>}
+
+            {/* <p>{features}</p> */}
+            <p>{features.map((feature) => <li>{feature}</li>)}</p>
+            <p>{features2?.b}</p>   {/* // Optional chaining to avoid errors if features2 is undefined */}
+           
+            {/* <h5>Product description</h5> */}
             </div>
     );
 }
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,10 +1,20 @@
 import Product from "./Product.jsx";
 function ProductTab() {
+  //  let options = [<li>"hi-tech"</li>, <li>"good quality"</li>, <li>"affordable"</li>];
+  //   let options2 = {a: "hi-tech", b: "good quality", c: "affordable"};
+  let options = ["hi-tech", "good quality", "affordable"];
     return (
   <> 
-  <Product/>
-  <Product/>
-  <Product/>
+  <Product
+   title="phone" 
+   price="30000" 
+//    features={["hi-tech", "good quality", "affordable"]}
+//    features2={{a: "hi-tech", b: "good quality", c: "affordable"}}/>
+
+ features={options}
+    />
+  <Product title="laptop" price={40000}/>
+  <Product title="pen"/>
 </>
  ); 
 }
